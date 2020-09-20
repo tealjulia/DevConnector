bab04029b2dd0730e6d9f4efa3ffb82e7256dffd
Teal Larson <larson.teal@gmail.com>
2020-09-20 11:07:17 -0700

    React components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Footer from './components/layout/Footer';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 function App() {
   return (
-    <div className="App">
-      <h1> My React App </h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
