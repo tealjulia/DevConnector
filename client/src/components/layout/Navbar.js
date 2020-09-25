cfc8f5f76df3bd64ebfa2066786cd94913111740
Teal Larson <larson.teal@gmail.com>
2020-09-25 12:59:40 -0700

    navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import logo from '../../img/connexion-app.svg';
 
 class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
     <div className="container">
-      <Link className="navbar-brand" to="/">DevConnector</Link>
+      <Link className="navbar-brand" to="/">
+        <img src={logo} alt="connexion logo" />
+      </Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
         <span className="navbar-toggler-icon"></span>
       </button>
